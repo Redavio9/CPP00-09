36c4ccd546bb1211ea07984175b3b74342238474
Reda Arraji <rarraji@c1r8p2.1337.ma>
2023-09-08 22:10:59 +0100

    reda

--- CPP02/ex02/Fixed.cpp
+++ CPP02/ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: rarraji <rarraji@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 11:13:41 by rarraji           #+#    #+#             */
-/*   Updated: 2023/09/07 12:44:53 by rarraji          ###   ########.fr       */
+/*   Updated: 2023/09/08 10:47:51 by rarraji          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ bool Fixed::operator==(const Fixed &c2) const
     return this->fixed_point == c2.fixed_point;
 }
 
-// -------------------------------------//
+// --------------- OPERATOR + - * / ----------------------//
 
 
 Fixed Fixed::operator+(const Fixed &c2)
@@ -65,6 +65,8 @@ Fixed Fixed::operator/(const Fixed &c2)
     return(c3);
 }
 
+// ----------------INCREMENTATION - DECREMENTATION ---------------------//
+
 Fixed &Fixed::operator++()
 {
     fixed_point++;
@@ -74,4 +76,45 @@ Fixed &Fixed::operator--()
 {
     fixed_point--;
     return(*this);
+}
+
+const Fixed Fixed::operator++(int) 
+{
+    Fixed tmp = *this;
+    fixed_point++;
+    return (tmp);
+}
+
+const Fixed Fixed::operator--(int) 
+{
+    Fixed tmp = *this;
+    fixed_point--;
+    return (tmp);
+}
+
+// ----------------INT MAX -- INT MIN---------------------//
+
+Fixed& Fixed::min(Fixed &fix1, Fixed &fix2)
+{
+	if (fix1 <= fix2)
+		return (fix1);
+	return (fix2);
+}
+Fixed& Fixed::max(Fixed &fix1, Fixed &fix2)
+{
+	if (fix1 >= fix2)
+		return (fix1);
+	return (fix2);
+}
+const Fixed& Fixed::min(const Fixed &fix1, const Fixed &fix2)
+{
+	if (fix1 <= fix2)
+		return (fix1);
+	return (fix2);
+}
+const Fixed& Fixed::max(const Fixed &fix1, const Fixed &fix2)
+{
+	if (fix1 >= fix2)
+		return (fix1);
+	return (fix2);
 }
--- CPP02/ex02/Fixed.hpp
+++ CPP02/ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: rarraji <rarraji@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 10:44:58 by rarraji           #+#    #+#             */
-/*   Updated: 2023/09/07 11:34:22 by rarraji          ###   ########.fr       */
+/*   Updated: 2023/09/08 10:45:17 by rarraji          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,13 @@ public:
     Fixed Fixed::operator*(const Fixed &c2);
     Fixed Fixed::operator/(const Fixed &c2);
     Fixed &Fixed::operator++();
+    const Fixed Fixed::operator++(int);
     Fixed &Fixed::operator--();
-    
+    const Fixed Fixed::operator--(int);
+    Fixed& Fixed::min(Fixed &fix1, Fixed &fix2);
+    Fixed& Fixed::max(Fixed &fix1, Fixed &fix2);
+    const Fixed& Fixed::min(const Fixed &fix1, const Fixed &fix2);
+    const Fixed& Fixed::max(const Fixed &fix1, const Fixed &fix2);
 };
 
 #endif
