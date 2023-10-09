9313bd218018ae261d45c9fadd46f8c910ef4575
Reda Arraji <rarraji@c1r3p8.1337.ma>
2023-10-09 10:59:29 +0100

    redaaa

--- CPP03/ex02/main.cpp
+++ CPP03/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rarraji <rarraji@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 11:44:43 by rarraji           #+#    #+#             */
-/*   Updated: 2023/10/06 12:04:56 by rarraji          ###   ########.fr       */
+/*   Updated: 2023/10/09 08:45:05 by rarraji          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 int main()
 {
   FragTrap obj("Sollix");
-
   obj.attack("reda");
   obj.beRepaired(9);
   obj.takeDamage(3);
